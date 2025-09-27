9d230a612888d2290e1c4f55221dbf2c764357f5
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2025-09-27 23:53:45 +0100

    updated testimonies

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,56 +2,83 @@
 export const testimonials = [
     {
         id: 1,
-        name: 'AYO ADEGBOYE',
+        name: 'DAVID ADEBAYO',
         position: 'CEO',
-        company: 'Arravo Ltd',
+        company: 'TechSolutions Ltd',
         image: '/assets/img/testimonials/Ayo-Adegboye-228x250.jpeg',
-        content: 'Working with Ganzy- Malgwi Technologis has been one of the best IT decisions my company made. Our IT project was executed in a timely and professional manner with lots of creativity and innovations brought into the solution. Thanks and keep up the great work team!',
+        content: 'Working with Ganzy-Malgwi Technologies has been one of the best IT decisions my company made. Our e-commerce platform was executed in a timely and professional manner with lots of creativity and innovations brought into the solution. Thanks and keep up the great work team!',
         rating: 5
     },
     {
         id: 2,
-        name: 'HAUWA HARUNA SHEIDI',
+        name: 'FUNKE ADEKOYA',
         position: 'Managing Director',
-        company: 'GTI Microfinance',
+        company: 'Prime Microfinance Bank',
         image: '/assets/img/testimonials/HAUWA-HARUNA-SHEIDI.jpeg',
-        content: 'We\'ve been working with JavaPlus Innovation for a few years now and they have consistently provided us with top-notch software development services. The team is highly knowledgeable, proactive and responsive, always ready to assist us with any challenges we encounter.',
+        content: 'We\'ve been working with Ganzy-Malgwi Technologies for a few years now and they have consistently provided us with top-notch software development services. The team is highly knowledgeable, proactive and responsive, always ready to assist us with any challenges we encounter.',
         rating: 5
     },
     {
         id: 3,
-        name: 'Chidinma Emekwa A',
+        name: 'CHIOMA NWOSU',
         position: 'Entrepreneur',
-        company: 'Tech Startup Founder',
+        company: 'EduTech Startup Founder',
         image: '/assets/img/testimonials/0579017.jpeg',
-        content: 'I was referred by a friend to consult Ganzy-Malgwi Technologies on an enterprise solution needed to be developed by my company. Initially I had a second thought contracting the project to JavaPlus but by the time the team was done with my solution, I was more than convinced that JavaPlus Innovations is the best software development company in Nigeria!',
+        content: 'I was referred by a friend to consult Ganzy-Malgwi Technologies on an enterprise solution needed to be developed by my company. Initially I had a second thought contracting the project but by the time the team was done with my solution, I was more than convinced that Ganzy-Malgwi Technologies is the best software development company in Nigeria!',
         rating: 5
     },
     {
         id: 4,
-        name: 'Dr. Tunde Johnson',
+        name: 'DR. KENNETH OKORO',
         position: 'CTO',
-        company: 'Healthcare Solutions Inc.',
+        company: 'MediCare Solutions Inc.',
         image: '/assets/img/testimonials/placeholder-1.jpg',
-        content: 'The healthcare management system developed by JavaPlus has revolutionized our patient care process. Their attention to detail and understanding of healthcare workflows is impressive.',
+        content: 'The healthcare management system developed by Ganzy-Malgwi Technologies has revolutionized our patient care process. Their attention to detail and understanding of healthcare workflows is impressive.',
         rating: 5
     },
     {
         id: 5,
-        name: 'Sarah Williams',
+        name: 'GRACE OLUWASEUN',
         position: 'Operations Manager',
-        company: 'Logistics Pro',
+        company: 'LogiMove Nigeria',
         image: '/assets/img/testimonials/placeholder-2.jpg',
-        content: 'Our logistics tracking system has seen a 40% improvement in efficiency since implementing JavaPlus\'s solution. Their team was professional and delivered ahead of schedule.',
+        content: 'Our logistics tracking system has seen a 40% improvement in efficiency since implementing Ganzy-Malgwi Technologies\' solution. Their team was professional and delivered ahead of schedule.',
         rating: 5
     },
     {
         id: 6,
-        name: 'Michael Chen',
+        name: 'BENJAMIN ADEKOYA',
         position: 'Financial Director',
-        company: 'Union Bank',
+        company: 'Heritage Microfinance',
         image: '/assets/img/testimonials/placeholder-3.jpg',
-        content: 'The custom banking software developed by JavaPlus has significantly improved our operational efficiency and customer experience. Their expertise in fintech is outstanding.',
+        content: 'The custom banking software developed by Ganzy-Malgwi Technologies has significantly improved our operational efficiency and customer experience. Their expertise in fintech is outstanding.',
+        rating: 5
+    },
+    {
+        id: 7,
+        name: 'AMARA CHUKWU',
+        position: 'Marketing Director',
+        company: 'Retail Plus Group',
+        image: '/assets/img/testimonials/placeholder-4.jpg',
+        content: 'The custom CRM system developed by Ganzy-Malgwi Technologies has transformed our customer engagement process. Their understanding of retail business needs is exceptional.',
+        rating: 5
+    },
+    {
+        id: 8,
+        name: 'SEGUN ADEWALE',
+        position: 'IT Manager',
+        company: 'Manufacturing Pro Ltd',
+        image: '/assets/img/testimonials/placeholder-5.jpg',
+        content: 'Our inventory management system developed by Ganzy-Malgwi Technologies has reduced operational costs by 30%. Their team demonstrated deep understanding of manufacturing processes.',
+        rating: 5
+    },
+    {
+        id: 9,
+        name: 'BISI ADEBAYO',
+        position: 'CEO',
+        company: 'AgroTech Solutions',
+        image: '/assets/img/testimonials/placeholder-6.jpg',
+        content: 'The farm management software created by Ganzy-Malgwi Technologies has helped us optimize our agricultural operations significantly. Their innovative approach to agritech is remarkable.',
         rating: 5
     }
 ];
