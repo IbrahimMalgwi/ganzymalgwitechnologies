811825ea5b5de0366d2df6786b181baa48529fa5
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2025-09-28 12:53:28 +0100

    updated portfolion

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -5,7 +5,7 @@ export const testimonials = [
         name: 'DAVID ADEBAYO',
         position: 'CEO',
         company: 'TechSolutions Ltd',
-        image: '/assets/img/testimonials/Ayo-Adegboye-228x250.jpeg',
+        image: '/assets/img/testimonials/testimonial1.jpg',
         content: 'Working with Ganzy-Malgwi Technologies has been one of the best IT decisions my company made. Our e-commerce platform was executed in a timely and professional manner with lots of creativity and innovations brought into the solution. Thanks and keep up the great work team!',
         rating: 5
     },
@@ -14,7 +14,7 @@ export const testimonials = [
         name: 'FUNKE ADEKOYA',
         position: 'Managing Director',
         company: 'Prime Microfinance Bank',
-        image: '/assets/img/testimonials/HAUWA-HARUNA-SHEIDI.jpeg',
+        image: '/assets/img/testimonials/testimonial2.jpg',
         content: 'We\'ve been working with Ganzy-Malgwi Technologies for a few years now and they have consistently provided us with top-notch software development services. The team is highly knowledgeable, proactive and responsive, always ready to assist us with any challenges we encounter.',
         rating: 5
     },
@@ -23,7 +23,7 @@ export const testimonials = [
         name: 'CHIOMA NWOSU',
         position: 'Entrepreneur',
         company: 'EduTech Startup Founder',
-        image: '/assets/img/testimonials/0579017.jpeg',
+        image: '/assets/img/testimonials/testimonial3.jpg',
         content: 'I was referred by a friend to consult Ganzy-Malgwi Technologies on an enterprise solution needed to be developed by my company. Initially I had a second thought contracting the project but by the time the team was done with my solution, I was more than convinced that Ganzy-Malgwi Technologies is the best software development company in Nigeria!',
         rating: 5
     },
@@ -32,7 +32,7 @@ export const testimonials = [
         name: 'DR. KENNETH OKORO',
         position: 'CTO',
         company: 'MediCare Solutions Inc.',
-        image: '/assets/img/testimonials/placeholder-1.jpg',
+        image: '/assets/img/testimonials/testimonial4.jpg',
         content: 'The healthcare management system developed by Ganzy-Malgwi Technologies has revolutionized our patient care process. Their attention to detail and understanding of healthcare workflows is impressive.',
         rating: 5
     },
@@ -41,7 +41,7 @@ export const testimonials = [
         name: 'GRACE OLUWASEUN',
         position: 'Operations Manager',
         company: 'LogiMove Nigeria',
-        image: '/assets/img/testimonials/placeholder-2.jpg',
+        image: '/assets/img/testimonials/testimonial5.jpg',
         content: 'Our logistics tracking system has seen a 40% improvement in efficiency since implementing Ganzy-Malgwi Technologies\' solution. Their team was professional and delivered ahead of schedule.',
         rating: 5
     },
@@ -50,7 +50,7 @@ export const testimonials = [
         name: 'BENJAMIN ADEKOYA',
         position: 'Financial Director',
         company: 'Heritage Microfinance',
-        image: '/assets/img/testimonials/placeholder-3.jpg',
+        image: '/assets/img/testimonials/testimonial6.jpg',
         content: 'The custom banking software developed by Ganzy-Malgwi Technologies has significantly improved our operational efficiency and customer experience. Their expertise in fintech is outstanding.',
         rating: 5
     },
@@ -59,7 +59,7 @@ export const testimonials = [
         name: 'AMARA CHUKWU',
         position: 'Marketing Director',
         company: 'Retail Plus Group',
-        image: '/assets/img/testimonials/placeholder-4.jpg',
+        image: '/assets/img/testimonials/testimonial7.jpg',
         content: 'The custom CRM system developed by Ganzy-Malgwi Technologies has transformed our customer engagement process. Their understanding of retail business needs is exceptional.',
         rating: 5
     },
@@ -68,7 +68,7 @@ export const testimonials = [
         name: 'SEGUN ADEWALE',
         position: 'IT Manager',
         company: 'Manufacturing Pro Ltd',
-        image: '/assets/img/testimonials/placeholder-5.jpg',
+        image: '/assets/img/testimonials/testimonial8.jpg',
         content: 'Our inventory management system developed by Ganzy-Malgwi Technologies has reduced operational costs by 30%. Their team demonstrated deep understanding of manufacturing processes.',
         rating: 5
     },
@@ -77,7 +77,7 @@ export const testimonials = [
         name: 'BISI ADEBAYO',
         position: 'CEO',
         company: 'AgroTech Solutions',
-        image: '/assets/img/testimonials/placeholder-6.jpg',
+        image: '/assets/img/testimonials/testimonial9.png',
         content: 'The farm management software created by Ganzy-Malgwi Technologies has helped us optimize our agricultural operations significantly. Their innovative approach to agritech is remarkable.',
         rating: 5
     }
